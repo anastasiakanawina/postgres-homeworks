fe4c4335036fa1bd5c093057f25bc2b87646e559
avkanavina <a.kanavina@skyeng.ru>
2023-08-21 22:43:06 +0500

    create new table and add new data

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    job_title text,
+	salary int
+);
+
+
+CREATE TABLE customers
+(
+    id int PRIMARY KEY,
+    age int,
+    name varchar(100) NOT NULL,
+    phone varchar(20) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE orders
+(
+    id serial PRIMARY KEY,
+    name_product text,
+	price int,
+	employees_id int,
+	customer_id int,
+	FOREIGN KEY (employees_id)  REFERENCES employees (id),
+	FOREIGN KEY (customer_id)  REFERENCES customers (id)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,45 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="password"
+)
+
+cur = conn.cursor()
+
+cur.execute("""
+                INSERT INTO employees VALUES
+                (1, 'Petr Petrov', 'sales', 40000),
+                (2, 'Maria Ivanova', 'manager', 80000),
+                (3, 'Oleg Kuznetzov', 'sales', 50000),
+                (4, 'Sergei Sergeev', 'sales', 90000),
+                (5, 'Petr Ivanov', 'manager', 70000);
+            """)
+
+conn.commit()
+
+cur.execute("""
+                INSERT INTO customers VALUES
+                (11, 33, 'Ivan', 89133456733),
+                (12, 42, 'Anastasia', 89352618490),
+                (13, 18, 'Mikhail', 89064537895),
+                (14, 26, 'Andrey', 89678038902);
+            """)
+
+conn.commit()
+
+cur.execute("""
+                INSERT INTO orders VALUES
+                (111, 'televizor', 20790, 4, 14),
+                (112, 'telefon', 34905, 4, 13),
+                (113, 'holodilnik', 52049, 1, 11),
+                (114, 'microvawe', 2309, 5, 12),
+                (115, 'telephone', 59867, 2, 14);
+            """)
+
+conn.commit()
+cur.close()
+conn.close()
