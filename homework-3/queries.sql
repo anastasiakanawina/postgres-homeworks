18dfbe56420ca62daa3843d289bc5f85c7a5e806
avkanavina <a.kanavina@skyeng.ru>
2023-09-01 22:09:42 +0500

    add hw 3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,15 +2,44 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT c.company_name, CONCAT(e.first_name, ' ', e.last_name) as fio
+FROM orders AS o
+LEFT JOIN customers AS c ON o.customer_id=c.customer_id
+LEFT JOIN employees AS e ON o.employee_id=e.employee_id
+LEFT JOIN shippers AS s ON o.ship_via=s.shipper_id
+WHERE 1=1
+	AND c.city = 'London'
+	AND e.city = 'London'
+	AND s.company_name = 'United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 
+SELECT p.product_name, p.units_in_stock, s.contact_name, s.phone
+FROM products as p
+LEFT JOIN categories as c ON p.category_id=c.category_id
+LEFT JOIN suppliers as s ON p.supplier_id=s.supplier_id
+WHERE discontinued != 1 and category_name in ('Dairy Products', 'Condiments')
+  		AND units_in_stock < 25
+ORDER BY p.units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
+SELECT c.company_name
+FROM customers AS c
+LEFT JOIN orders AS o ON o.customer_id=c.customer_id
+WHERE order_id is null
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+
+SELECT DISTINCT product_name
+FROM (
+	SELECT *
+	FROM order_details as od
+	LEFT JOIN products as pr on od.product_id=pr.product_id
+	WHERE quantity=10) as sq
+
+-- не совсем понятно зачем тут подзапрос?
